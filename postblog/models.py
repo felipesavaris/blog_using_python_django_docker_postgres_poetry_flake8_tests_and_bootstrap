c9daf54fbe7bbc7aee24e25492d3d968d963ef5a
Felipe Savaris <fesavaris@gmail.com>
2021-06-20 19:29:09 -0300

    add model definition attributes

--- postblog/models.py
+++ postblog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+# from ckeditor.fields import RichTextField
 
-# Create your models here.
+
+class PostBlog(models.Model):
+
+    title = models.CharField(max_length=60)
+    # description = RichTextField()
+    descriptiom = models.TextField() # blank=True
