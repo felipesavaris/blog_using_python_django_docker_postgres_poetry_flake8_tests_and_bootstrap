94259ac37d74a3aff971a225f263b77f1803c91f
Felipe Savaris <fesavaris@gmail.com>
2021-06-20 22:04:58 -0300

    add base in all templates and create method create post

--- postblog/urls.py
+++ postblog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.list_all_posts, name='list_all_posts'),
+    path('create', views.create_post, name='create_post'),
     path('update/<int:id>', views.update_post, name='update_post'),
     path('delete/<int:id>', views.delete_post, name='delete_post'),
 ]
--- postblog/views.py
+++ postblog/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import PostBlog
+from postblog.form import PostBlogForms
 
 
 def list_all_posts(request):
     posts = PostBlog.objects.all()
     return render(request, 'list_all_posts.html', {'posts': posts})
+
+
+def create_post(request):
+    form = PostBlogForms(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('list_all_posts')
+    return render(request, 'form.html', {'form': form})
 
 
 def update_post(request):
