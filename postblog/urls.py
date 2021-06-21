5e8eab175960781b655aacdc5591b062436a9ba8
Felipe Savaris <fesavaris@gmail.com>
2021-06-20 21:23:01 -0300

    create tamplate, view and urls for listing posts data in blog

--- postblog/urls.py
+++ postblog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list_all_posts, name='list_all_posts'),
+    path('update/<int:id>', views.update_post, name='update_post'),
+    path('delete/<int:id>', views.delete_post, name='delete_post'),
 ]
--- postblog/views.py
+++ postblog/views.py
@@ -5,4 +5,12 @@ from .models import PostBlog
 
 def list_all_posts(request):
     posts = PostBlog.objects.all()
-    return render(request, 'list_all_posts', {'posts': posts})
+    return render(request, 'list_all_posts.html', {'posts': posts})
+
+
+def update_post(request):
+    pass
+
+
+def delete_post(request):
+    pass
